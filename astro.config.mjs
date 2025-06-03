d6dccdce45365e5b5704ad7159c91ade44cb426d
thanksstevenkim <109639347+thanksstevenkim@users.noreply.github.com>
2025-06-03 20:05:32 +0900

    Rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import path from "path";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
